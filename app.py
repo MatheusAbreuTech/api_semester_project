cee58ccf6c422b9924810c03a784dbc02d803d2d
Matheus <matheusabreutech@gmail.com>
2025-03-15 19:15:11 -0300

    feat: creating basic config

--- app.py
+++ app.py
@@ -1,30 +1,6 @@
-from flask import Flask, jsonify
-
-app = Flask(__name__)
-
-professores = [
-        {"id": 1, "nome": "Ana Silva", "disciplina": "Matemática"},
-        {"id": 2, "nome": "Carlos Souza", "disciplina": "História"},
-        {"id": 3, "nome": "Fernanda Costa", "disciplina": "Biologia"}
-    ]
-
-alunos =  [
-        {"id": 1, "nome": "João Pereira", "idade": 15, "turma_id": 101},
-        {"id": 2, "nome": "Mariana Lima", "idade": 14, "turma_id": 101},
-        {"id": 3, "nome": "Lucas Oliveira", "idade": 16, "turma_id": 102},
-        {"id": 4, "nome": "Beatriz Santos", "idade": 15, "turma_id": 103},
-        {"id": 5, "nome": "Gabriel Martins", "idade": 14, "turma_id": 103}
-    ]
-
-turmas = [
-        {"id": 101, "nome": "Turma A", "ano": 2024, "professor_id": 1},
-        {"id": 102, "nome": "Turma B", "ano": 2024, "professor_id": 2},
-        {"id": 103, "nome": "Turma C", "ano": 2024, "professor_id": 3}
-    ],
-
-@app.route('/teachers')
-def hello_world():
-    return jsonify({'professores': professores})
+from config import app
+from routes.alunos_routes import alunos_blueprint
+app.register_blueprint(alunos_blueprint)
 
 if __name__ == '__main__':
-    app.run()
+  app.run(host=app.config["HOST"], port = app.config['PORT'],debug=app.config['DEBUG'] )
