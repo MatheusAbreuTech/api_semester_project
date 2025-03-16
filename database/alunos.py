70197e0f9f42bdf868c45ed618a75343f18652f2
kayquesanmartin <kayque.sanmartin65@gmail.com>
2025-03-15 22:01:17 -0300

    fix: data arranged in directory

---
+++ database/alunos.py
@@ -0,0 +1,7 @@
+alunos =  [
+        {"id": 1, "nome": "João Pereira", "idade": 15, "turma_id": 101},
+        {"id": 2, "nome": "Mariana Lima", "idade": 14, "turma_id": 101},
+        {"id": 3, "nome": "Lucas Oliveira", "idade": 16, "turma_id": 102},
+        {"id": 4, "nome": "Beatriz Santos", "idade": 15, "turma_id": 103},
+        {"id": 5, "nome": "Gabriel Martins", "idade": 14, "turma_id": 103}
+]
