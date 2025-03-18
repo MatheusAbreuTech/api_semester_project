5ebaafbe4884cbf5f3190307dc02260255abcdce
Matheus Abreu <matheusabreutech@gmail.com>
2025-03-18 19:24:37 -0300

    fix: remove config file

--- app.py
+++ app.py
@@ -260,6 +260,5 @@ def cadastra_aluno_turma(turma_id, aluno_id):
         'message': f'aluno cadastrado com sucesso na turma {turma_id}'
     })
 
-
 if __name__ == '__main__':
-  app.run(host=app.config["HOST"], port = app.config['PORT'],debug=app.config['DEBUG'] )
+    app.run(debug=True)
