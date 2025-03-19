b3a4bfdd4413b1c8d783d762dbd9d539a4ea1415
kayquesanmartin <kayque.sanmartin65@gmail.com>
2025-03-19 10:33:50 -0300

    feat: unittest for post aluno

--- app_test.py
+++ app_test.py
@@ -23,6 +23,26 @@ class TestAPP(unittest.TestCase):
         response = self.app.get('/alunos/999')
         self.assertEqual(response.status_code, 404)
 
+    def test_create_aluno(self):
+        novo_aluno = {
+            "nome": "Teste Aluno",
+            "idade": 17,
+            "turma_id": 1
+        }
+        response = self.app.post('/alunos', data=json.dumps(novo_aluno), content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('message', json.loads(response.data))
+
+    def test_create_aluno_dados_invalidos(self):
+        aluno_invalido = {
+            "nome": "Sem Idade"
+        }
+        response = self.app.post('/alunos', json=aluno_invalido)
+        self.assertEqual(response.status_code, 400)
+
+        data = json.loads(response.data)
+        self.assertIn("erro", data)
+
 
 if __name__ == '__main__':
     unittest.main()
