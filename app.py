a73680791112781ef0af94b4dcf34cd56fb531b8
igor <ii.igoralmeida@gmail.com>
2025-04-08 20:46:31 -0300

    feat: creation of professor model

--- app.py
+++ app.py
@@ -4,78 +4,10 @@ from aluno.aluno_controller import alunos_blueprint
 from database.alunos import alunos
 from database.professores import professores
 from database.turmas import turmas
+from professor.professor_controller import professores_blueprint
 
 app.register_blueprint(alunos_blueprint)
-
-@app.route('/professores', methods=['GET'])
-def get_professores():
-    data = professores
-    if data is None:
-        return jsonify([])
-    return jsonify(professores)
-
-@app.route('/professor/<int:professor_id>', methods=['GET'])
-def get_professor(professor_id):
-    for professor in professores:
-        if professor['id'] == professor_id:
-            return jsonify({
-                'professor': professor
-            })
-
-    return jsonify({'error': 'professor nao encontrado'}), 404
-
-@app.route('/professor', methods=['POST'])
-def create_professor():
-    data = request.json
-    if 'nome' not in data:
-        return jsonify({'error': 'nome é um campo obrigatório'}), 400
-
-    if 'disciplina' not in data:
-        return jsonify({'error': 'disciplina é um campo obrigatório'}), 400
-
-    professor = {
-        'id': len(professores) + 1,
-        'nome': data['nome'],
-        'disciplina': data['disciplina']
-    }
-    professores.append(professor)
-    return jsonify({
-        'message': 'professor criado com sucesso',
-        'professor': professor
-    })
-
-
-@app.route('/professor/<int:professor_id>', methods=['PUT'])
-def update_professor(professor_id):
-    for professor in professores:
-        if professor['id'] == professor_id:
-            data = request.json
-            if 'nome' not in data:
-                return jsonify({'error': 'nome é um campo obrigatório'}), 400
-
-            if 'disciplina' not in data:
-                return jsonify({'error': 'disciplina é um campo obrigatório'}), 400
-
-            professor.update(data)
-            return jsonify({
-                'message': 'professor atualizado com sucesso',
-                'professor': professor
-            })
-
-    return jsonify({'error': 'professor nao encontrado'}), 404
-
-
-@app.route('/professor/<int:professor_id>', methods=['DELETE'])
-def delete_professor(professor_id):
-    for professor in professores:
-        if professor['id'] == professor_id:
-            professores.remove(professor)
-            return jsonify({
-                'message': 'professor removido com sucesso'
-            })
-
-    return jsonify({'error': 'professor nao encontrado'}), 404
-
+app.register_blueprint(professores_blueprint)
 
 @app.route('/turmas', methods=['GET'])
 def get_turmas():
---
+++ professor/professor_model.py
@@ -0,0 +1,61 @@
+from flask import jsonify
+from flask import request
+
+from database.professores import professores
+
+class ProfessorModel:
+    def __init__(self):
+        self.professores = professores
+
+    def create_professor(self, data):
+        professor = {
+            "id": len(self.professores) + 1,
+            "nome": data["nome"],
+            "disciplina": data["disciplina"]
+        }
+
+        self.professores.append(professor)
+        return jsonify({
+            "message": "Professor criado com sucesso!",
+            "professor": professor
+        })
+
+
+    def get_professor(self,professor_id):
+        for professor in self.professores:
+            if professor["id"] == professor_id:
+                return jsonify({"professor": professor}), 200
+            return jsonify({'error': 'aluno não encontrado'}),400
+
+    def get_professores(self):
+        return jsonify({"professores": self.professores}), 200
+
+    def update_professor(self,professor_id,data):
+        for professor in professores:
+            if professor['id'] == professor_id:
+                data = request.json
+                if 'nome' not in data:
+                    return jsonify({'error': 'nome é um campo obrigatório'}), 400
+
+                if 'disciplina' not in data:
+                    return jsonify({'error': 'disciplina é um campo obrigatório'}), 400
+
+                professor.update(data)
+                return jsonify({
+                    'message': 'professor atualizado com sucesso',
+                    'professor': professor
+                })
+    def delete_professor(self,professor_id):
+        for professor in professores:
+            if professor['id'] == professor_id:
+                professores.remove(professor)
+                return jsonify({
+                    'message': 'professor removido com sucesso'
+                })
+
+        return jsonify({'error': 'professor nao encontrado'}), 404
+
+
+
+
+
