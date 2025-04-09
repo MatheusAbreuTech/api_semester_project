6b2059cf308c42968286f21635f38866df519712
Artur <tunuartur@gmail.com>
2025-04-09 11:05:20 -0300

    feat: created one route for the post method of classes

--- turma/turma_controller.py
+++ turma/turma_controller.py
@@ -11,3 +11,8 @@ def get_turmas():
 @turmas_blueprint.route("/turmas/<int:turma_id>", methods=['GET'])
 def get_turma(turma_id):
     return turmas_model.get_turma(turma_id)
+
+@turmas_blueprint.route("/turmas", methods=['POST'])
+def create_turma():
+    data = request.json
+    return turmas_model.create_turma(data)
