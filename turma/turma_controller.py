fa69d67a8eb59358367cfe41b2a6445553dccfd5
Artur <tunuartur@gmail.com>
2025-04-09 11:02:07 -0300

    feat: created two routes for the get method of classes

---
+++ turma/turma_controller.py
@@ -0,0 +1,13 @@
+from flask import (Blueprint, request)
+from turma.turma_model import TurmaModel
+
+turmas_blueprint = Blueprint('turmas', __name__)
+turmas_model = TurmaModel()
+
+@turmas_blueprint.route('/turmas', methods=['GET'])
+def get_turmas():
+    return turmas_model.get_turmas()
+
+@turmas_blueprint.route("/turmas/<int:turma_id>", methods=['GET'])
+def get_turma(turma_id):
+    return turmas_model.get_turma(turma_id)
